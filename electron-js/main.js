273c19e7952b9c6df530d7fd87a571d3fddd5d81
Ved Panse <vedkpanse@gmail.com>
2024-05-14 18:46:14 -0700

    edit

--- electron-js/main.js
+++ electron-js/main.js
@@ -1,31 +1,40 @@
-const { app, BrowserWindow } = require('electron/main')
-const path = require('node:path')
+import { app, BrowserWindow } from 'electron';
+import { fileURLToPath } from 'node:url'; // Import fileURLToPath function to convert import.meta.url to a file path
+import path from 'node:path';
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     transparent: true,
+    vibrancy: {
+      theme: 'dark',
+      effect: 'acrylic',
+      disableOnBlur: true,
+    },
+    backgroundColor: '#00000000',
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(fileURLToPath(import.meta.url), 'preload.js') // Use fileURLToPath to convert import.meta.url to a file path
     }
-  })
+  });
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+}).catch(error => {
+  console.error('Error starting the app:', error);
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
